80c5eb30c54779d87ca49647b4513df2dc8d1cb4
ViktorThink <viktor.bowallius@hotmail.com>
2023-01-04 12:15:12 +0100

    g

--- test_QnA_v4.py
+++ test_QnA_v4.py
@@ -82,7 +82,7 @@ def general_process(prompt, dialog, model_name):
             dialog_text = dialog_text + "AI: " + dialog[i]+"\n"
     
     prompt=prompt.strip() + "\n" + dialog_text.strip() + "\nAI:"
-    # print("prompt\n",prompt)
+    print("prompt\n",prompt)
     reply = openai.Completion.create(
     model=model_name,
     prompt=prompt,
@@ -147,6 +147,6 @@ def test_QnA(path, openai_key, num_replies=4, minimum_similarity=0.85,preprocess
             print("Post processed reply:", reply)
         else:
             reply = general_process(general_prompt, dialog, model_name)
-            print("General answer", reply)
+            print("General answer:", reply)
         dialog.append(reply)
             
